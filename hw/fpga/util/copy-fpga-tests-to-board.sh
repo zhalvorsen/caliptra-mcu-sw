f11359bab6ac01278e0d7979f948c38e0eb15dfe
Timothy Trippel <5633066+timothytrippel@users.noreply.github.com>
2025-08-26 20:30:08 -0700

    [fpga] remove scripts for dispatching FPGA tests (#379)
    These shell scripts have been replaced by an xtask flow in #355, #357,
    and #361.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- hw/fpga/util/copy-fpga-tests-to-board.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Licensed under the Apache-2.0 license
-
-rsync -avzP \
-  target/all-fw.zip \
-  target/aarch64-unknown-linux-gnu/debug/xtask \
-  caliptra-test-bins.tar.zst \
-  "hw/fpga/util/run-fpga-tests.sh" \
-  mcu-host:"$USER"
